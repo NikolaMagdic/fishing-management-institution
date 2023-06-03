2b908ada34e42236dd450d0b0708ed2818bc2123
NikolaMagdic <57407543+NikolaMagdic@users.noreply.github.com>
2023-06-03 16:20:49 +0200

    Backend application initialization

---
+++ fishing-management-backend/src/main/java/com/example/fishingmanagementbackend/FishingManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.fishingmanagementbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishingManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishingManagementBackendApplication.class, args);
+	}
+
+}
---
+++ fishing-management-backend/src/test/java/com/example/fishingmanagementbackend/FishingManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fishingmanagementbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishingManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
